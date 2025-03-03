f8257fdc8ab0fcc209734f3f740123fe86a5b688
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-04 00:32:25 +0800

    KernelSU: manual security hooks
    Since 3.10 / 3.18 does not have security_add_hooks and LSM_HOOK_INIT, we
    have to manually hook functions on security/security.c.
    
    These macro/fucntions only got merged at Linux 4.2-rc1.
    It is probably too much to backport.
    
    reference:
    	security_add_hooks - https://github.com/torvalds/linux/commit/b1d9e6b0646d0e5ee5d9050bd236b6c65d66faef
    	LSM_HOOK_INIT - https://github.com/torvalds/linux/commit/e20b043a6902ecb61c2c84355c3bae5149f391db
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- security/security.c
+++ security/security.c
@@ -132,6 +132,15 @@ int __init register_security(struct security_operations *ops)
 	return 0;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
+		     unsigned long arg4, unsigned long arg5);
+extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
+extern int ksu_handle_setuid(struct cred *new, const struct cred *old);
+extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
+			      unsigned perm);
+#endif
+
 /* Security operations */
 
 int security_binder_set_context_mgr(struct task_struct *mgr)
@@ -545,6 +554,9 @@ int security_inode_mknod(struct inode *dir, struct dentry *dentry, umode_t mode,
 int security_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_rename(old_dentry, new_dentry);
+#endif
         if (unlikely(IS_PRIVATE(old_dentry->d_inode) ||
             (new_dentry->d_inode && IS_PRIVATE(new_dentry->d_inode))))
 		return 0;
@@ -879,6 +891,9 @@ int security_kernel_module_from_file(struct file *file)
 int security_task_fix_setuid(struct cred *new, const struct cred *old,
 			     int flags)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_setuid(new, old);
+#endif
 	return security_ops->task_fix_setuid(new, old, flags);
 }
 
@@ -958,6 +973,9 @@ int security_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 	rc = yama_task_prctl(option, arg2, arg3, arg4, arg5);
 	if (rc != -ENOSYS)
 		return rc;
+#endif
+#ifdef CONFIG_KSU
+	ksu_handle_prctl(option, arg2, arg3, arg4, arg5);
 #endif
 	return security_ops->task_prctl(option, arg2, arg3, arg4, arg5);
 }
@@ -1440,6 +1458,9 @@ void security_key_free(struct key *key)
 int security_key_permission(key_ref_t key_ref,
 			    const struct cred *cred, key_perm_t perm)
 {
+#ifdef CONFIG_KSU
+	ksu_key_permission(key_ref, cred, perm);
+#endif
 	return security_ops->key_permission(key_ref, cred, perm);
 }
 
