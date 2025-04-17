a2d27cdcb41414be76fa7f0c8efe8982eb181d6f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-04-18 02:52:39 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -108,8 +108,8 @@ if [ "$TOOLCHAIN" == gcc ]; then
 	if [ ! -d "$HOME/gcc64" ] && [ ! -d "$HOME/gcc32" ]
 	then
 		echo -e "$green << cloning gcc from arter >> \n $white"
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 "$HOME"/gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm "$HOME"/gcc32
+		git clone --depth=1 https://github.com/malkist01/malkist-toolchain -b master "$HOME"/gcc64
+		git clone --depth=1 https://github.com/malkist01/arm-eabi-4.9.git -b master "$HOME"/gcc32
 	fi
 	export PATH="$HOME/gcc64/bin:$HOME/gcc32/bin:$PATH"
 	export STRIP="$HOME/gcc64/aarch64-elf/bin/strip"
